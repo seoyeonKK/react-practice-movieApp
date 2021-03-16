57eccdb9b6caf1b2b27ffe7d3bb73278b10d9b90
권서연 <seoyeon@seoyeon-ui-MacBookPro.local>
2021-03-16 17:41:17 +0900

    Practice react component & JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+// function Potato({ fav }) { //  object 내부 property 직접 접근 가능 
+function Potato(props) {
+  console.log(props); // 로그에 props object 확인 가능
+  console.log(props.fav);
+  return <h1>I like {props.fav}</h1>;
+  //  return <h1>I like {fav}</h1>; // props를 fav로 넘겨받았으면 property 직접 접근 가능 
 }
 
+// react component (html을 반환하는 함수)
+function App() {
+  return <div>
+      <h1>Hello</h1>
+      {/* <'Potato component' 'Property' ='Value'> */}
+      <Potato fav ="gamza" >
+        {/* something = {true} 
+        papapa={["ddd", 1, 2, 3]} /> // 다양한 포맷 가능  */}
+      </Potato>
+      <Potato fav = "goguma"/>
+      <Potato fav = "sweet potato" />
+    </div>
+} 
+
 export default App;
+   
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+  
 ReactDOM.render(
+  // react component 를 보여줌 
+  // JSX 개념 React에서 (javascript + HTML)  조합 
+  // React application은 App component 하나만을 렌더링함 
+  // 따라서, App 안에 다른 component들을 넣어야 된다. 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+); 
